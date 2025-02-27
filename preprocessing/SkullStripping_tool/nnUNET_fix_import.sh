2e8908b13438464ebbd1b37216e4b2efac4d05a2
PT_PinedaG <65619025+gusSCIMOV@users.noreply.github.com>
2025-02-27 10:20:48 -0600

    Add files via upload

--- preprocessing/SkullStripping_tool/nnUNET_fix_import.sh
+++ preprocessing/SkullStripping_tool/nnUNET_fix_import.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# Path might need to be adjusted based on your Python environment
+file_path="/usr/local/lib/python3.10/dist-packages/nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py"
+
+# Check if the file exists
+if [ -f "$file_path" ]; then
+  # Use sed to replace the import statement
+  sudo sed -i 's/from torch import GradScaler/from torch.cuda.amp import GradScaler/' "$file_path"
+  echo "Fixed GradScaler import in nnUNetTrainer.py line 40 in ${file_path}"
+else
+  echo "File not found: $file_path"
+fi
